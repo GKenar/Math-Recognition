18e21091640dae7faa12dca8bfdd163b923ded94
GKenar <kenar.gp@gmail.com>
2021-02-11 15:56:51 +0200

    Рефакторинг symbols_tree_adapter.py

--- symbols_tree_adapter.py
+++ symbols_tree_adapter.py
@@ -6,28 +6,40 @@ def adapt_to_solver(s: Symbol):
     return ''.join(__adapt_to_solver(s))
 
 
+def fraction_handling(s: Symbol, super, subsc, above, below):
+    list = []
+    list.extend('(')
+    list.extend(above)
+    list.extend(')/(')
+    list.extend(below)
+    list.extend(')')
+
+    return list
+
+
 def __adapt_to_solver(s: Symbol):
     list = []
     while s is not None:
+        list_super = adapt_to_solver(s.super)
+        list_subsc = adapt_to_solver(s.subsc)
+        list_above = adapt_to_solver(s.above)
+        list_below = adapt_to_solver(s.below)
+
         if s.symbol_label == Symbols.SYMBOL_FRACTION:
-            list.extend('(')
-            list.extend(adapt_to_solver(s.above))
-            list.extend(')/(')
-            list.extend(adapt_to_solver(s.below))
-            list.extend(')')
-            s = s.next  # Переделать
-            continue
+            list.extend(fraction_handling(s, list_super, list_subsc, list_above, list_below))
+        else:
+            list.append(symbol_to_str(s.symbol_label))
 
-        list.append(symbol_to_str(s.symbol_label))
-
-        if s.super is not None:
-            list.extend('^(')
-            list.extend(adapt_to_solver(s.super))
-            list.extend(')')
-        if s.subsc is not None:
-            list.extend('_(')
-            list.extend(adapt_to_solver(s.subsc))
-            list.extend(')')
+            if s.super is not None:
+                list.extend('^(')
+                list.extend(adapt_to_solver(s.super))
+                list.extend(')')
+            if s.subsc is not None:
+                list.extend('_(')
+                list.extend(adapt_to_solver(s.subsc))
+                list.extend(')')
 
         s = s.next
     return list
+
+
