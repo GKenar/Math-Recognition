172ea7a5c73639b7fdcfcb18423a7be96eca49d9
GKenar <kenar.gp@gmail.com>
2021-05-28 16:01:08 +0300

    Добавлена адаптация для нового типа данных.

--- solver.py
+++ solver.py
@@ -22,5 +22,11 @@ def solver_output_to_str(data):
             return '{0}/{1}'.format(data.args[0], data.args[1])
         else:
             return str(data)
+    elif type(data) is tuple:
+        str_builder = ''
+        for d in data:
+            str_builder = str_builder + '{0} = {1}; '.format(d[0].args[0], d[0].args[1])
+
+        return str_builder
 
     return str(data)
